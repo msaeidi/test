38cbf59ddaf33d335d01b29b356b180a48339b49
mo <mohammadsaeedi@hotmail.com>
2014-12-29 01:12:16 -0800

    2nd commit

--- src/main/java/matchcraft/IntegerOperator.java
+++ src/main/java/matchcraft/IntegerOperator.java
@@ -6,17 +6,29 @@ import java.util.List;
 public class IntegerOperator implements Operator{
 
 	@Override
-	public Integer value(List<Integer> t1, List<Integer> t2) {
-		Integer t1max = t1.max();
-		Integer t2max = t2.max();
-		return t1max > t2max ? t1max : t2max;
-	}
-
-	@Override
-	public <E> List<Integer> value(List<Integer> c1, List<Integer> c2) {
+	public <E> List<E> value(List<E> c1, List<E> c2) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public <E> E getValue(List<E> c1, List<E> c2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+//	@Override
+//	public Integer value(List<Integer> t1, List<Integer> t2) {
+//		Integer t1max = t1.max();
+//		Integer t2max = t2.max();
+//		return t1max > t2max ? t1max : t2max;
+//	}
+
+//	@Override
+//	public List<Integer> value(List<Integer> c1, List<Integer> c2) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
 
 
 }
--- src/main/java/matchcraft/Operator.java
+++ src/main/java/matchcraft/Operator.java
@@ -1,9 +1,9 @@
 package matchcraft;
 
-import java.util.Collection;
 import java.util.List;
 
 public interface Operator {
 	//public <E> Collection<E> value(Collection<E> c1, Collection<E> c2);
 	public <E> List<E> value(List<E> c1, List<E> c2);
+	public <E> E getValue(List<E> c1, List<E> c2);
 }
